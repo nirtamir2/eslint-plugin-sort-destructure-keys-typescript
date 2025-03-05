83fb5291bd308121bc24bd385b5cff47b0617164
nirtamir2 <nirtamir2@gmail.com>
2025-03-05 14:48:09 +0200

    return cjs build (#12)

--- build.config.ts
+++ build.config.ts
@@ -4,5 +4,8 @@ export default defineBuildConfig({
   entries: ["src/index", "src/config", "src/types"],
   declaration: true,
   clean: true,
+  rollup: {
+    emitCJS: true,
+  },
   externals: ["@typescript-eslint/utils", "typescript"],
 });
